fb2eba07a0ea31ce843dbfb12413b86aa2d556c5
rominafarhad <kameliafarhad@gmail.com>
2026-02-22 08:30:47 -0800

    implement exit builtin

--- app/main.py
+++ app/main.py
@@ -1,17 +1,18 @@
 import sys
 
 def main():
-    # Infinite loop to keep the shell running
     while True:
-        # Display the prompt symbol
         sys.stdout.write("$ ")
         sys.stdout.flush()
 
         # Wait for user input
         command = input()
 
-        # Print error message for any command entered
-        if command:
+        # Check if the command is 'exit 0'
+        if command == "exit 0":
+            sys.exit(0)
+        # If it's any other command, show error
+        elif command:
             print(f"{command}: command not found")
 
 if __name__ == "__main__":
