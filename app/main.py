d276ce341aa70ff049f1db7a12cbbc462efb94f0
rominafarhad <kameliafarhad@gmail.com>
2026-02-22 07:42:34 -0800

    add exit command

--- app/main.py
+++ app/main.py
@@ -4,8 +4,13 @@ def main():
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
+        
         command = input()
-        print(f"{command}: command not found")
+
+        if command == "exit 0":
+            sys.exit(0)
+        else:
+            print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
