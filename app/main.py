140eabc08c30bbb3a489f4d8feb692ab209b64f7
rominafarhad <kameliafarhad@gmail.com>
2026-02-22 08:56:02 -0800

    implement echo builtin

--- app/main.py
+++ app/main.py
@@ -10,11 +10,17 @@ def main():
         except EOFError:
             break
 
-        # Handle the 'exit' command
-        # This will catch both 'exit' and 'exit 0'
+        # Check if the command starts with 'exit'
         if command == "exit 0" or command == "exit":
             sys.exit(0)
-            
+
+        # Check if the command starts with 'echo '
+        elif command.startswith("echo "):
+            # We take everything after the first 5 characters ('echo ')
+            message = command[5:]
+            print(message)
+
+        # For any other unknown command
         elif command:
             print(f"{command}: command not found")
 
