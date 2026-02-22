76812679bc9c4c8a2cdd9d7ccd655d8bab1e790a
rominafarhad <kameliafarhad@gmail.com>
2026-02-22 07:39:33 -0800

    pass stage 1

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 import sys
 
-
 def main():
-    # TODO: Uncomment the code below to pass the first stage
-    # sys.stdout.write("$ ")
-    pass
-
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+        command = input()
+        print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
